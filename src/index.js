7b555f46cfe15918de589ab43acdc45e85da57db
thuyhoang11 <gianglamweb@gmail.com>
2023-06-09 17:11:22 +0700

    commit

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "../src/scenes/login/Login"
+import Signup from "../src/scenes/signup/Signup"
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path={"/*"} element={<App />} />
+        <Route path={"/login"} element={<Login />} />
+        <Route path={"/signup"} element={<Signup />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
+);
